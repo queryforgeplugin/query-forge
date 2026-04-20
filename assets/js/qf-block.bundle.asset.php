5b97c4ebe2366c5848c5786d5db2a4a53fb5a05a
Query Forge Plugin <info@queryforgeplugin.com>
2026-04-20 12:32:10 +0200

    Added Single Page Templates
    New Feature > Single Page Templates. See read me.

--- assets/js/qf-block.bundle.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '6d95b7fbe1894e51a5e3');
--- assets/js/qf-editor.bundle.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '0e4f21902e58ab2232e2');
